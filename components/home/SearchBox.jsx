3f8e974279d7ad0a81af1f8cd38521497f52034e
Shafayet <11shafayet@gmail.com>
2024-03-20 20:00:04 +0600

    select and car dummy image added

--- components/home/SearchBox.jsx
+++ components/home/SearchBox.jsx
@@ -1,19 +1,23 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import Select from 'react-select';
+import Select, { StylesConfig } from 'react-select';
 
 const makeList = [
   {
+    value: 'Chevrolet',
     label: 'Chevrolet',
   },
   {
+    value: 'Nissan',
     label: 'Nissan',
   },
   {
+    value: 'Cadillac',
     label: 'Cadillac',
   },
   {
+    value: 'Dodge',
     label: 'Dodge',
   },
 ];
@@ -34,8 +38,8 @@ const modelList = [
 ];
 
 const SearchBox = () => {
-  const [make, setMake] = useState('');
-  const [model, setModel] = useState('');
+  const [make, setMake] = useState(null);
+  const [model, setModel] = useState(null);
 
   useEffect(() => {
     console.log(make);
@@ -44,19 +48,55 @@ const SearchBox = () => {
   const handleSubmit = () => {
     console.log(make);
   };
+
+  const colourStyles = {
+    control: (styles) => ({
+      ...styles,
+      backgroundColor: 'white',
+    }),
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+      return {
+        ...styles,
+        backgroundColor: isSelected
+          ? '#0C6135'
+          : isFocused
+          ? '#E0EBE4'
+          : undefined,
+        color: isSelected ? '#fff' : '#000',
+
+        ':active': {
+          ...styles[':active'],
+          backgroundColor: isSelected ? '#0C6135' : 'white',
+        },
+      };
+    },
+  };
   return (
     <section className="py-12 md:py-16 bg-primary">
       <div className="container px-4 mx-auto">
         <form
-          action=""
           className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-8 lg:gap-12 max-w-[900px] mx-auto"
+          onSubmit={handleSubmit}
         >
+          {/* make */}
           <Select
-            className="basic-single"
-            classNamePrefix="select"
-            defaultValue={makeList[0]}
-            name="make"
+            className="leading-9"
+            defaultValue={make}
             options={makeList}
+            placeholder="Select Make"
+            name="make"
+            styles={colourStyles}
+            onChange={(e) => setMake(e.label)}
+          />
+          {/* model */}
+          <Select
+            className="leading-9"
+            defaultValue={model}
+            options={modelList}
+            placeholder="Select Model"
+            name="model"
+            styles={colourStyles}
+            onChange={(e) => setModel(e.label)}
           />
 
           <button
