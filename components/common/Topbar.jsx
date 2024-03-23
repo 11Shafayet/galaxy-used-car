eb27bd37c83af0050afc3009a6c4a22cff05f339
Shafayet <11shafayet@gmail.com>
2024-03-23 19:41:48 +0600

    topbar updated

--- components/common/Topbar.jsx
+++ components/common/Topbar.jsx
@@ -1,11 +1,15 @@
+'use client';
+
 import {
   FaFacebookF,
   FaInstagram,
   FaLinkedinIn,
   FaPhoneAlt,
+  FaUser,
 } from 'react-icons/fa';
 import { CiClock2 } from 'react-icons/ci';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const socialItems = [
   {
@@ -23,6 +27,9 @@ const socialItems = [
 ];
 
 const Topbar = () => {
+  const [userLoggedIn, setUserLoggedIn] = useState(true);
+  const [userDash, setUserDash] = useState(true);
+
   return (
     <div className="py-3">
       <div className="container mx-auto px-4">
@@ -48,18 +55,38 @@ const Topbar = () => {
                 </Link>
               ))}
             </div>
-
-            <div className="flex items-center text-sm leading-none">
-              <Link
-                href="/sign-in"
-                className="pr-3 border-r hover:text-primary"
-              >
-                Login
-              </Link>
-              <Link href="sign-up" className="pl-3 hover:text-primary">
-                Register
-              </Link>
-            </div>
+            {!userLoggedIn ? (
+              <div className="flex items-center text-sm leading-none">
+                <Link
+                  href="/sign-in"
+                  className="pr-3 border-r hover:text-primary"
+                >
+                  Login
+                </Link>
+                <Link href="sign-up" className="pl-3 hover:text-primary">
+                  Register
+                </Link>
+              </div>
+            ) : (
+              <div className="relative">
+                <div
+                  className="w-11 h-11 text-xl flex justify-center items-center bg-gray-300 rounded-full cursor-pointer relative"
+                  onClick={() => setUserDash((prev) => !prev)}
+                >
+                  <FaUser />
+                  {userDash && (
+                    <div className="absolute top-[111%] right-0 bg-gray-100 shadow-light min-w-60 flex flex-col justify-center items-center z-[10000]">
+                      <h6 className="hover:bg-primary hover:text-white cursor-pointer font-bold text-lg w-full py-3 px-3">
+                        Dashboard
+                      </h6>
+                      <h6 className="hover:bg-primary hover:text-white cursor-pointer font-bold text-lg w-full py-3 px-3">
+                        Logout
+                      </h6>
+                    </div>
+                  )}
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
